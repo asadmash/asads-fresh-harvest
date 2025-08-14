10aa89721bf9c05551d0ccaefa3ad23191461bfe
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-14 17:25:38 +0600

    rtk api slice base setup

---
+++ src/features/api/apiSlice.ts
@@ -0,0 +1,17 @@
+// base api slice setup
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://code-commando.com/api/v1",
+    prepareHeaders: (headers, {getState}) => {
+        const token = (getState() as any).auth.token;
+        if(token){
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+        return headers;
+    },
+   }),
+   tagTypes: ['Product', 'User'],
+  endpoints: () => ({}),
+});
