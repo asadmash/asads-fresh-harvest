d8a84a660112edd01ba71ad6578700c52f74e168
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-14 07:54:14 +0600

    complete hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,34 +3,84 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section className="h-screen w-full bg-[url(/mobile-hero-bg.png)] lg:bg-[url(/desktop-hero-bg.png)] @container bg-cover relative z-10 bg-right">
+    <section className="h-auto w-full bg-[url(/mobile-hero-bg.png)] lg:bg-[url(/desktop-hero-bg.png)] @container bg-cover relative z-10 bg-right">
       <div className="container_inner z-30">
         {/* texts */}
         <div className=" w-full sm:w[400px] md:w-[500px] mt-30 md:mt-50 left-0">
           <p className="p-2 bg-lime text-lime w-fit rounded-2xl font-semibold">
             Welcome to Fresh Harvest
           </p>
-          <h1 className="text-2xl text-black sm:text-4xl md:text-6xl py-4 md:py-8 font-bold">Fresh Fruits and Vegetables</h1>
+          <h1 className="text-2xl text-black sm:text-4xl md:text-6xl py-4 md:py-8 font-bold">
+            Fresh Fruits and Vegetables
+          </h1>
           <p>
             At Fresh Harvest, we are passionate about providing you with the
             freshest and most flavorful fruits and vegetables
           </p>
-          <button className="bg-orange mt-10 px-3 md:px-6 py-2 md:py-4 font-bold text-xl md:text-2xl text-white rounded-xl">Shop Now</button>
+          <button className="bg-orange mt-10 px-3 md:px-6 py-2 md:py-4 font-bold text-xl md:text-2xl text-white rounded-xl">
+            Shop Now
+          </button>
         </div>
 
         {/* offer card */}
         <div className=" ml-5 md:ml-20 mt-5 flex">
           <div className="hidden md:block">
-            <Image src={`/arrow.png`} width={100} height={100} alt="arrow" className="w-[100px] h-[100px]" />
+            <Image
+              src={`/arrow.png`}
+              width={100}
+              height={100}
+              alt="arrow"
+              className="w-[100px] h-[100px]"
+            />
           </div>
           <div className="flex px-2 py-2 bg-grey-card text-sm xsm:text-lg md:text-xl rounded-2xl items-center gap-2">
             <div>
               <p className="text-green">Special Offer</p>
               <h2>Fresh Salad</h2>
-              <p><span className="text-green">Up to </span><strong>70% </strong> off</p>
+              <p>
+                <span className="text-green">Up to </span>
+                <strong>70% </strong> off
+              </p>
             </div>
             <div>
-              <Image src={`/salad-2.png`} width={100} height={100} alt="salad" className="w-[50px] h-[50px] xsm:w-[80px] xsm:h-[80px] sm:w-[100px] sm:h-[100px]" />
+              <Image
+                src={`/salad-2.png`}
+                width={100}
+                height={100}
+                alt="salad"
+                className="w-[50px] h-[50px] xsm:w-[80px] xsm:h-[80px] sm:w-[100px] sm:h-[100px]"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* app store */}
+        <div className=" pt-5 pb-10 mt-10">
+          <p className="mb-5">Download app:</p>
+          <div className="flex gap-10">
+            <div className="apple-store flex p-4 bg-[#000] text-white rounded-2xl cursor-pointer">
+              <Image
+                src={`/brand-apple.svg`}
+                width={50}
+                height={50}
+                alt="apple-logo"
+              />
+              <div>
+                <p>Download on the</p>
+                <h2 className="text-2xl">App Store</h2>
+              </div>
+            </div>
+            <div className="play-store flex p-4 bg-[#000] text-white rounded-2xl cursor-pointer">
+              <Image
+                src={`brand-google-play.svg`}
+                width={50}
+                height={50}
+                alt="play-logo"
+              />
+              <div>
+                <p className="uppercase">get it on</p>
+                <h2 className="text-2xl">Google Play</h2>
+              </div>
             </div>
           </div>
         </div>
