195b06eff177cc0970cc8d6314ede63f265c5c11
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-14 22:46:19 +0600

    shape of Product

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: string;
+  productName: string;
+  description: string;
+  price: number;
+  images: string[];
+  stock: number;
+  categoryId: string;
+  isDeleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
