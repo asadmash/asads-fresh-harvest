f3a13a6530da27bff94f8ccf9357a2a286838fc6
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-14 22:51:00 +0600

    allow remote url to render api's image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.ibb.co.com", "upload.wikimedia.org"], // add your image host here
+  },
 };
 
 export default nextConfig;
