aa47005ce79c34380d8d71ebc0e8613a8b87bb1d
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-14 22:49:49 +0600

    data fetching logic

--- src/features/api/productApi.ts
+++ src/features/api/productApi.ts
@@ -1,21 +1,27 @@
 // import base api slice and product type
 import { apiSlice } from "./apiSlice";
-import {Product} from "@types/product";
+import { Product } from "@/types/product";
+
+interface ApiResponse {
+  data: Product[];
+  message: string;
+  success: boolean;
+}
 
 export const productApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        // get all products
-        getProducts: builder.query<Product[], void>({
-            query: () => '/products',
-            providesTags: ['Product'],
-        }),
-        // get products by id
-        getProductById: builder.query<Product, string>({
-             query: (id) => `/products/${id}`,
-      providesTags: (result, error, id) => [{ type: 'Product', id }],
-        }),
-    })
-})
+  endpoints: (builder) => ({
+    // get all products
+    getProducts: builder.query<ApiResponse, void>({
+      query: () => `/products`,
+      providesTags: ["Product"],
+    }),
+    // get products by id
+    getProductById: builder.query<Product, string>({
+      query: (id) => `/products/${id}`,
+      providesTags: (result, error, id) => [{ type: "Product", id }],
+    }),
+  }),
+});
 
 // export hooks
 export const { useGetProductsQuery, useGetProductByIdQuery } = productApi;
