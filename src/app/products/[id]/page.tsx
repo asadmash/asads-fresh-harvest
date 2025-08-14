7fad62d69b75c637d45245c1787aaf300529d4d1
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-15 02:15:41 +0600

    fix params issue

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
 import ProductPageClient from "./productPageClient";
-
+// update params
 export default function Page({ params }: { params: { id: string } }) {
   return <ProductPageClient id={params.id} />;
 }
