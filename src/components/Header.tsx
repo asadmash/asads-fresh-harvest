ed53956e0b68d1aba85e6d262c8805923a0c7821
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-15 19:44:46 +0600

    a toggle able sidebar for mobile device

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { FiHeart } from "react-icons/fi";
 import { HiOutlineShoppingBag } from "react-icons/hi";
-// import SearchBar from "./SearchBar";
-// import UserAvatar from "./UserAvatar";
-// import { useStateValue } from "../context/StateContext";
-// import CartSidebar from "./CartSidebar";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 import Navbar from "./Navbar";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -51,25 +53,74 @@ const Header = () => {
             </Link>
           </div>
           <div className="flex items-center gap-10">
-            <Navbar />
-            <div className="hidden lg:flex text-gray-500 text-[30px] gap-8 items-center">
-              <div className="relative">
-                <FiHeart className="text-white" />
+            <div className="hidden sm:block">
+              <Navbar />
+            </div>
+            <div className="flex items-center gap-5 text-white">
+              <div className="relative hidden sm:block">
+                <FiHeart className="text-3xl" />
                 <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">
                   {/* {state.favorites.length} */}
                 </div>
               </div>
               <div className="relative cursor-pointer">
-                <HiOutlineShoppingBag className="text-white" />
+                <HiOutlineShoppingBag className="text-3xl" />
                 <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">
                   {/* {state.cart.length} */}
                 </div>
+              </div>
+              <div className="sm:hidden">
+                <button onClick={toggleSidebar}>
+                  {isSidebarOpen ? (
+                    <svg
+                      className="w-6 h-6 text-white"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <GiHamburgerMenu className="text-white text-2xl" />
+                  )}
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* {isCartOpen && <CartSidebar onClose={toggleCart} />} */}
+      {isSidebarOpen && (
+        <div className="fixed top-0 left-0 h-full w-64 bg-[#ff6a19] shadow-lg p-5 sm:hidden z-50">
+          <ul className="flex flex-col gap-5 mt-10 font-normal text-xl">
+            <li className="relative">
+              <Link href="/" className="nav_link">
+                Home
+              </Link>
+            </li>
+            <li className="relative">
+              <Link href="/" className="nav_link">
+                Shop
+              </Link>
+            </li>
+            <li className="relative">
+              <Link href="/" className="nav_link">
+                About us
+              </Link>
+            </li>
+            <li className="relative">
+              <Link href="/" className="nav_link">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,71 +1,16 @@
 "use client";
 import Link from 'next/link'
-import React, { useState } from 'react'
-import { GiHamburgerMenu } from "react-icons/gi";
+import React from 'react'
 
 export default function Navbar() {
-    const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-    const toggleSidebar = () => {
-        setSidebarOpen(!isSidebarOpen);
-    };
   return (
     <div>
-        <ul className='hidden md:flex gap-4 md:gap-5 lg:gap-10 font-normal text-xl'>
+        <ul className='flex gap-4 sm:gap-5 md:gap-10 font-normal text-xl'>
             <li  className='relative'><Link href="/" className='nav_link' >Home</Link></li>
             <li  className='relative'><Link href="/" className='nav_link' >Shop</Link></li>
             <li  className='relative'><Link href="/" className='nav_link' >About us</Link></li>
             <li  className='relative'><Link href="/" className='nav_link' >Blog</Link></li>
         </ul>
-        <div className="md:hidden">
-        <button onClick={toggleSidebar} className="md:hidden">
-          {isSidebarOpen ? (
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              ></path>
-            </svg>
-          ) : (
-            <GiHamburgerMenu />
-          )}
-        </button>
-      </div>
-
-      {isSidebarOpen && (
-        <div className="fixed top-0 left-0 h-full w-64 bg-[#ff6a19] shadow-lg p-5 md:hidden">
-          <ul className="flex flex-col gap-5 mt-10 font-normal text-xl">
-            <li className="relative">
-              <Link href="/" className="nav_link">
-                Home
-              </Link>
-            </li>
-            <li className="relative">
-              <Link href="/" className="nav_link">
-                Shop
-              </Link>
-            </li>
-            <li className="relative">
-              <Link href="/" className="nav_link">
-                About us
-              </Link>
-            </li>
-            <li className="relative">
-              <Link href="/" className="nav_link">
-                Blog
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
     </div>
   )
 }
