570d1a6fec31d53a47cf884851ee8f20cd1712c7
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-15 18:27:32 +0600

    update the endpoint to match with api doc

--- src/features/auth/authApi.ts
+++ src/features/auth/authApi.ts
@@ -1,22 +1,30 @@
 // import the base api setup
-import { apiSlice } from '../api/apiSlice';
+import { apiSlice } from "../api/apiSlice";
 
 // extend base api with auth endports
 export const authApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     // login
-    login: builder.mutation<{ token: string }, { email: string; password: string }>({
-      query: (credentials) => ({ //a post request to auth/login with creadentials
-        url: '/auth/login',
-        method: 'POST',
+    login: builder.mutation<
+      { token: string },
+      { email: string; password: string }
+    >({
+      query: (credentials) => ({
+        //a post request to auth/login with creadentials
+        url: "/auth/login",
+        method: "POST",
         body: credentials,
       }),
     }),
     // signup
-    signup: builder.mutation<{ token: string }, { email: string; password: string }>({
-      query: (credentials) => ({//a post request to auth/signup with creadentials
-        url: '/auth/signup',
-        method: 'POST',
+    signup: builder.mutation<
+      { token: string },
+      { email: string; password: string }
+    >({
+      query: (credentials) => ({
+        //a post request to users/register with creadentials
+        url: "/users/register",
+        method: "POST",
         body: credentials,
       }),
     }),
