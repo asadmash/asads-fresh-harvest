7e5ffd4cd2f860c5bd59d81ce77905aee46721bf
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-15 21:32:55 +0600

    add fav and cart slice to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,12 +4,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "@/features/api/apiSlice";
 import authReducer from "@/features/auth/authSlice";
+import cartReducer from '@/features/cart/cartSlice';
+import favReducer from '@/features/fav/favSlice';
 // import uiReducer from "@/features/ui/uiSlice";
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
+          cart: cartReducer,
+    fav: favReducer,
         // ui: uiReducer,
     },
     middleware: (getDefaultMiddleware) => 
