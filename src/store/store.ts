f762853bcb6ea499726bcb57ec6656f17ceb9cf4
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-16 01:27:41 +0600

    modify store to user redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,25 +1,57 @@
 // Redux store with rtk query
 
 // imports
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { apiSlice } from "@/features/api/apiSlice";
 import authReducer from "@/features/auth/authSlice";
-import cartReducer from '@/features/cart/cartSlice';
-import favReducer from '@/features/fav/favSlice';
+import cartReducer from "@/features/cart/cartSlice";
+import favReducer from "@/features/fav/favSlice";
 // import uiReducer from "@/features/ui/uiSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  PersistPartial,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer,
-          cart: cartReducer,
-    fav: favReducer,
-        // ui: uiReducer,
-    },
-    middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat(apiSlice.middleware),
+// Combine reducers
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+  fav: favReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
 });
 
+// Persist config
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // only persist auth slice
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//Configure store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // ignore redux-persist actions for serializable check
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
+});
+
+//Create persistor
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
