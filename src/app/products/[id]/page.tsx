43b7bbb9ae28ab175d9d3edffa7a52c030187b57
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-15 11:13:02 +0600

    structure the details page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -6,6 +6,9 @@ import { useAppSelector } from "@/hooks/reduxHooks";
 import AuthModal from "@/components/AuthModal";
 import { section } from "motion/react-client";
 import Image from "next/image";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import ProductCard from "@/components/ProductCard";
 
 export default function ProductDetailsPage() {
   const params = useParams();
@@ -26,8 +29,8 @@ export default function ProductDetailsPage() {
   return (
     <section className="@container">
       <div className="container_inner">
-        <div className="fruites">
-          <div>
+        <section className="fruites">
+          <div className="product-image w-full md:w-[50%]">
             <Image
               src={
                 product.images && product.images.length > 0
@@ -39,7 +42,60 @@ export default function ProductDetailsPage() {
               alt="product-image"
             />
           </div>
-        </div>
+          <div>
+            <p>Fruits</p>
+            <h2>Coconut</h2>
+            <div className="starCount">
+              <p>5.0(1 review)</p>
+            </div>
+            <div className="weight">$6.3/kg</div>
+            <p className="product-desc">
+              From our farm directly to your door, our fresh coconuts are
+              harvested at the peak of ripeness, offering you a sweet, hydrating
+              treat full of flavor. Packed with natural nutrients, coconut is
+              perfect for a variety of culinary uses, from smoothies to savory
+              dishes, or even for a refreshing drink straight from the shell.
+            </p>
+            <div className="quantity flex gap-2">
+              <h3>Quantity</h3>
+              <div className="box">
+                <span className="decrese">-</span>
+                <strong>1</strong>
+                <span className="increase">+</span>
+              </div>
+              <span>/kg</span>
+            </div>
+
+            <div className="fav-cart">
+              <div className="add-fav">
+                <FaHeart />
+                Save as favorite
+              </div>
+              <div className="add-cart">
+                <FaShoppingCart />
+                Add to cart
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="details">
+<div className="navigator">
+  <div>Description</div>
+  <div>Reviews</div>
+</div>
+<div className="description">
+
+Our coconuts are sustainably grown, ensuring the best quality and taste. Each coconut is handpicked and carefully prepared, offering you the freshest product possible. Rich in healthy fats, electrolytes, and essential nutrients, coconuts provide both hydration and nourishment. Whether you're using the water, flesh, or milk, our coconuts bring versatility to your kitchen while supporting healthy living.
+Perfect for smoothies, desserts, curries, and more - let the natural sweetness of the coconut elevate your recipes. Enjoy the tropical goodness in its purest form, directly from nature.
+</div>
+        </section>
+        <section className="related-products">
+          <p>Our Poducts</p>
+          <h2>Related Products</h2>
+          <div className="product-container">
+           product
+          </div>
+        </section>
       </div>
     </section>
   );
