1ba29fedf47e692ff9e5dad9ee26d52ab4ff9403
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-16 13:51:11 +0600

    refactor apislice

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -4,15 +4,16 @@ import { RootState } from "@/store/store";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://code-commando.com/api/v1",
-    prepareHeaders: (headers, {getState}) => {
-        const token = (getState() as RootState).auth.token;
-        if(token){
-            headers.set('Authorization', `Bearer ${token}`);
-        }
-        return headers;
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://code-commando.com/api/v1",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
     },
-   }),
-   tagTypes: ['Product', 'User',],
+  }),
+  tagTypes: ["Product", "User"],
   endpoints: () => ({}),
 });
